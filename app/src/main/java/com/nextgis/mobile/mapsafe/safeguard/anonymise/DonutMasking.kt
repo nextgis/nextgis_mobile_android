422ba22f86f4e639feeacd437f92c807e051ff2e
PankajSharma <pankajeshwara@gmail.com>
2026-05-24 02:04:52 +1200

    Add MapSafe donut masking package

---
+++ app/src/main/java/com/nextgis/mobile/mapsafe/safeguard/anonymise/DonutMasking.kt
@@ -0,0 +1,11 @@
+package com.nextgis.mobile.mapsafe.safeguard.anonymise
+
+/**
+ * Placeholder entry point for MapSafe donut masking.
+ *
+ * Existing masking logic will progressively be migrated here from
+ * temporary integration code.
+ */
+object DonutMasking {
+    const val VERSION = "0.1"
+}
