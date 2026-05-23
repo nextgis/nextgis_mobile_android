e7f79ae0b916ae82aa2d300fececdadfb02ff993
PankajSharma <pankajeshwara@gmail.com>
2026-05-24 02:05:02 +1200

    Add MapSafe hexabinning package

---
+++ app/src/main/java/com/nextgis/mobile/mapsafe/safeguard/anonymise/Hexabinning.kt
@@ -0,0 +1,10 @@
+package com.nextgis.mobile.mapsafe.safeguard.anonymise
+
+/**
+ * Placeholder entry point for H3 hexabinning support.
+ *
+ * Future implementation will use the h3-java library.
+ */
+object Hexabinning {
+    const val VERSION = "0.1"
+}
