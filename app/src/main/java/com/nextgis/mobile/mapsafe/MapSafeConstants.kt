3c29474a84157b9b10e06aec8f94884b9d93f38c
PankajSharma <pankajeshwara@gmail.com>
2026-05-24 02:17:34 +1200

    Add MapSafe shared constants

---
+++ app/src/main/java/com/nextgis/mobile/mapsafe/MapSafeConstants.kt
@@ -0,0 +1,15 @@
+package com.nextgis.mobile.mapsafe
+
+/**
+ * Shared constants for the MapSafe mobile module.
+ */
+object MapSafeConstants {
+    const val METHOD_DONUT_MASKING = "donut_masking"
+    const val METHOD_HEXABINNING = "hexabinning"
+    const val METHOD_ENCRYPT = "encrypt"
+    const val METHOD_INTEGRITY_RECORD = "integrity_record"
+
+    const val DEFAULT_H3_RESOLUTION = 8
+    const val MASKED_LAYER_SUFFIX = "_masked"
+    const val HEXBIN_LAYER_SUFFIX = "_hexbin"
+}
